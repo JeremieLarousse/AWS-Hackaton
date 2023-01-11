36d058204c489dd112376098901b3a55ca78fb5c
Philippe Berra <berraphilippe07@gmail.com>
2023-01-11 12:27:17 +0100

    update 11/01 12:30

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-import '../styles/Homepage.scss';
 
+import "../../styles/homepage.css";
 const Homepage = () => {
     return (
-        <div className='Homepage'>
-            <NavLink to='mission'>
-                <button type='submit'>Mission</button>
-            </NavLink>
-            <NavLink to='destination'>
-                <button type='submit'>Destination</button>
-            </NavLink>
-            <NavLink to='garage'>
-                <button type='submit'>Garage</button>
-            </NavLink>
+
+
+        <div class="loop-wrapper">
+            <div class="mountain"></div>
+            <div class="hill"></div>
+            <div class="tree"></div>
+            <div class="tree"></div>
+            <div class="tree"></div>
+            <div class="rock"></div>
+            <div class="truck"></div>
+            <div class="wheels"></div>
+
+            <div className='Homepage'>
+                <NavLink to='mission'>
+                    <button type='submit'>Mission</button>
+                </NavLink>
+                <NavLink to='destination'>
+                    <button type='submit'>Destination</button>
+                </NavLink>
+                <NavLink to='garage'>
+                    <button type='submit'>Garage</button>
+                </NavLink>
+            </div>
         </div>
     );
 };
