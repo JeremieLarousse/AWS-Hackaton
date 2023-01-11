099120feb10e666c874fc42f2cb29de1f6aa4b37
vincentRouget <vincent.rouget86@gmail.com>
2023-01-11 16:18:42 +0100

    Style page garage opérationnel

--- frontend/src/components/PageGarage.jsx
+++ frontend/src/components/PageGarage.jsx
@@ -1,28 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from "react-router-dom";
 import CardVehicule from './CardVehicule';
 import '../styles/PageGarage.scss';
+import TiArrowBack from '@meronex/icons/ti/TiArrowBack';
+
+const array = [
+    {
+        marque: 'opel',
+        modèle: 'corsa'
+    },
+    {
+        marque: 'bmw',
+        modèle: 's3'
+    },
+    {
+        marque: 'bmw',
+        modèle: 's3'
+    },
+    {
+        marque: 'bmw',
+        modèle: 's3'
+    },
+    {
+        marque: 'bmw',
+        modèle: 's3'
+    },
+];
+
+
 
 const PageGarage = () => {
+
+    useEffect(() => {
+        console.log('test');
+    }, [])
+
     return (
         <div className='container'>
-            <div className='return'>
-                <NavLink to='/'>
-                    <button type='submit'>Retour</button>
-                </NavLink>
-            </div>
-            <div className='title'>
-                <h1>GARAGE</h1>
-            </div>
-            <div className='options'>
-                <p>Mission : /mission</p>
-                <p>Destination : /destination (/100km)</p>
-            </div>
-            <div className='pool'>
-                <CardVehicule />
-            </div>
-            <div className='save'>
-                <button type='submit' className='button'>Enregistrer</button>
+            <img src='https://cdn.pixabay.com/photo/2018/08/25/22/32/autos-3631086_1280.jpg' alt='Background' className='background'></img>
+            <div className='page'>
+                <div className='return'>
+                    <NavLink to='/'>
+                        <button type='submit' className='button_return'><TiArrowBack /></button>
+                    </NavLink>
+                </div>
+                <div className='title'>
+                    <h1>Parc Automobile</h1>
+                </div>
+                <div className='options'>
+                    <p>Mission : /mission</p>
+                    <p>Destination : /destination (/100km)</p>
+                </div>
+                <div className='pool'>
+                    {array.map((element) => {
+                        return (
+                            <div>
+                                <CardVehicule marque={element.marque} modèle={element.modèle} />
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className='save'>
+                    <button type='submit' className='button'>Enregistrer</button>
+                </div>
             </div>
         </div>
     );
