f7bf943fe8815d14b1dfdd984295ae3862fd865c
vincentRouget <vincent.rouget86@gmail.com>
2023-01-11 18:50:29 +0100

    Axios + filtre + map ok

--- frontend/src/components/PageGarage.jsx
+++ frontend/src/components/PageGarage.jsx
@@ -4,31 +4,11 @@ import axios from 'axios';
 import CardVehicule from './CardVehicule';
 import '../styles/PageGarage.scss';
 import TiArrowBack from '@meronex/icons/ti/TiArrowBack';
-
-// const array = [
-//     {
-//         marque: 'opel',
-//         modèle: 'corsa'
-//     },
-//     {
-//         marque: 'bmw',
-//         modèle: 's3'
-//     },
-//     {
-//         marque: 'bmw',
-//         modèle: 's3'
-//     },
-//     {
-//         marque: 'bmw',
-//         modèle: 's3'
-//     },
-//     {
-//         marque: 'bmw',
-//         modèle: 's3'
-//     },
-// ];
+import GiSave from '@meronex/icons/gi/GiSave';
+import { element } from 'prop-types';
 
 const PageGarage = () => {
+    // Voitures :
     const [marque, setMarque] = useState('Marque inconnue');
     const [modele, setModele] = useState('Modèle inconnu');
     const [immatriculation, setImmatriculation] = useState('Immatriculation inconnue');
@@ -36,6 +16,13 @@ const PageGarage = () => {
     const [date, setDate] = useState('Date inconnue');
     const [disponible, setDisponible] = useState('Disponibilité inconnue');
     const [autonomie, setAutonomie] = useState('Autonomie inconnue');
+
+    // Mission et destination :
+    const [mission, setMission] = useState('Livraison');
+    const [destination, setDestination] = useState('Sud');
+    const [distance, setDistance] = useState(0);
+
+    // Axios :                                      
     const [data, setData] = useState([]);
 
     const displayVoiture = () => {
@@ -43,10 +30,11 @@ const PageGarage = () => {
             .get(`${import.meta.env.VITE_BACKEND_URL}/parc`)
             .then((res) => {
                 setData(res.data);
-                console.log(res.data);
             })
-    }
-
+    };
+    // const handleDistance = (e) => {
+    //     setDistance(e.target.value);
+    // };
     useEffect(() => {
         displayVoiture();
     }, [])
@@ -55,36 +43,45 @@ const PageGarage = () => {
         <div className='container'>
             <img src='https://cdn.pixabay.com/photo/2018/08/25/22/32/autos-3631086_1280.jpg' alt='Background' className='background'></img>
             <div className='page'>
-                <div className='return'>
-                    <NavLink to='/'>
-                        <button type='submit' className='button_return'><TiArrowBack /></button>
-                    </NavLink>
-                </div>
-                <div className='title'>
-                    <h1>Parc Automobile</h1>
-                </div>
-                <div className='options'>
-                    <p>Mission : /mission</p>
-                    <p>Destination : /destination (/100km)</p>
+                <div className='fixe'>
+                    <div className='return'>
+                        <NavLink to='/'>
+                            <button type='submit' className='button_return'><TiArrowBack /></button>
+                        </NavLink>
+                    </div>
+                    <div className='title'>
+                        <h1>Parc Automobile</h1>
+                    </div>
+                    <div className='options'>
+                        <div className='texte'>
+                            <p>Mission : {mission}</p>
+                            <p>Destination : {destination} ({distance}km)</p>
+                        </div>
+                        {/* <input onChange={handleDistance}></input> */}
+                        <div className='save'>
+                            <button type='submit' className='button'><GiSave /></button>
+                        </div>
+                    </div>
                 </div>
                 <div className='pool'>
-                    {data.map((element) => {
-                        return (
-                            <div>
-                                <CardVehicule
-                                    marque={element.marque}
-                                    modele={element.modele}
-                                    immatriculation={element.immatriculation}
-                                    type={element.type}
-                                    date={element.date}
-                                    disponible={element.disponible}
-                                    autonomie={element.autonomie} />
-                            </div>
+                    {data
+                        .filter((e) =>
+                            (e.autonomie >= distance)
                         )
-                    })}
-                </div>
-                <div className='save'>
-                    <button type='submit' className='button'>Enregistrer</button>
+                        .map((element) => {
+                            return (
+                                <div>
+                                    <CardVehicule
+                                        marque={element.marque}
+                                        modele={element.modele}
+                                        immatriculation={element.immatriculation}
+                                        type={element.type}
+                                        date={element.date}
+                                        disponible={element.disponible}
+                                        autonomie={element.autonomie} />
+                                </div>
+                            )
+                        })}
                 </div>
             </div>
         </div>
