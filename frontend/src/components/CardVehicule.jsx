e2ecf488873d8a66a2b3cceb8c70a9486468dd75
vincentRouget <vincent.rouget86@gmail.com>
2023-01-13 09:15:30 +0100

    Version finale

--- frontend/src/components/CardVehicule.jsx
+++ frontend/src/components/CardVehicule.jsx
@@ -13,7 +13,7 @@ const CardVehicule = ({ image, marque, modele, immatriculation, type, date, disp
                 <p><span className='categorie'>Immatriculation :</span> {immatriculation}</p>
                 <p><span className='categorie'>Type :</span> {type}</p>
                 <p><span className='categorie'>Date de mise en circulation :</span> {date}</p>
-                <p><span className='categorie'>Disponible :</span> {disponible}</p>
+                <p><span className='categorie'>Disponible :</span> {disponible ? 'oui' : 'non'}</p>
                 <p><span className='categorie'>Autonomie :</span> {autonomie}</p>
             </div>
         </div>
